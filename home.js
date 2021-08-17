6166a02dfb7b69fea1dfbb3a613434987705b017
TjoAlex <alle.98@live.se>
2021-08-17 12:32:45 +0000

    Tried to implement a function for the carousel section in the home page, found out there was a more simple soultion by using boostrap library and instead  removed what I tried as for Javascript solution

--- home.js
+++ home.js
@@ -11,3 +11,31 @@ function parallax(e) {
         translateY(${y}px)`
     })
 }
+
+//Countdown timer 
+//code from Jayesh Goyan https://stackoverflow.com/questions/31559469/how-to-create-a-simple-javascript-timer
+var myVar = setInterval(function () {
+    myTimer()
+}, 1000);
+var secondlimit = 30;
+
+function myTimer() {
+    if (secondlimit == 0) {
+        myStopFunction();
+    }
+
+    document.getElementById("safeTimerDisplay").innerHTML = '00:' + zeroPad(secondlimit, 2);
+    secondlimit = secondlimit - 1;
+
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
+
+function zeroPad(num, places) {
+    var zero = places - num.toString().length + 1;
+    return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+//Carousel 
