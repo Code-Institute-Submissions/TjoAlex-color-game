cce00b94517b42fdfc533a67760a0d1a708f64c2
TjoAlex <alle.98@live.se>
2021-08-21 13:09:55 +0000

    Putting javascript into assets folder

---
+++ assets/game.js
@@ -0,0 +1,87 @@
+//Array of 6 colors
+var colors = generateRandomColors(6);
+//List of variables
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var square1 = document.querySelector(".square1"); //Tag to add a result button that change color
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function () {
+    //generat all new colors
+    colors = generateRandomColors(6);
+    //pick new random color from array
+    pickedColor = pickColor();
+    //change color display to match picked color
+    colorDisplay.textContent = pickedColor;
+    this.textContent = "Get new rgb"
+
+    messageDisplay.textContent = "";
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue"; //must change for h1 heading color
+})
+
+colorDisplay.textContent = pickedColor;
+
+//give squares different colors
+for (var i = 0; i < squares.length; i++) {
+    //Add initial colors to squares
+    squares[i].style.backgroundColor = colors[i]
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        //Grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //Compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again!" //should tell player to play again
+            changeColors(clickedColor);
+            square1.style.backgroundColor = clickedColor; // change h1 to square1
+        } else {
+            this.style.backgroundColor = "#232323"
+            messageDisplay.textContent = "Try Again"
+        }
+
+    });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    //make array
+    var arr = []
+    //repeat num times
+    for (var i = 0; i < num; i++) {
+        //get random color and push into array
+        arr.push(randomColor())
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor() {
+    //Pick a "red" from 0-255
+    var r = Math.floor(Math.random() * 256);
+    //Pick a "green" from 0-255
+    var g = Math.floor(Math.random() * 256);
+    //Pick a "blue" from 0-255
+    var b = Math.floor(Math.random() * 256);
+    "rgb(r,g,b)"
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
